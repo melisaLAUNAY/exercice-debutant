cb5cec60805aee0b794e10f2674ec86488ea9683
melisaLAUNAY <46750116+melisaLAUNAY@users.noreply.github.com>
2019-01-17 13:53:50 +0100

    Add files via upload

---
+++ devine le mdp.py
@@ -0,0 +1,20 @@
+deviner="caca"
+print("Une bombe est prête à exploser!!!!")
+print("Il faut deviner le mot de passe et vite...")
+deviner=input("donne le mdp?")
+while deviner!="caca":
+    if deviner=="caca":
+        break
+    print("Indice: il sort de ton anus")
+    deviner=input("Vite, entre le mot de passe!")
+    if deviner!="caca":
+        print("Booooooooooooooooooooom !!!!")
+        print("Recommence!")
+    else:
+        print("bravo tu as gagné")
+
+ 
+        
+    
+    
+    
